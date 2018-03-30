c5ebdc9baae9225d3737319316dd22ef40e5c8c1
Matthew Bobke <mcbobke@gmail.com>
2018-03-30 02:21:41 -0700

    Formatting and a directory check

--- Install-WinOpenSSH.ps1
+++ Install-WinOpenSSH.ps1
@@ -1,33 +1,38 @@
 [CmdletBinding()]
 Param()
 
-#### TODO ####
-# Replace .NET web request with Powershell Invoke-Webrequest
-# Figure out why the WebClient object isn't being disposed of properly
-#   If this script is run twice without restarting Powershell, the download hangs
+# Check to see if the C:\temp\ directory exists - create it if it doesn't
+if (!(Test-Path -Path "C:\temp" -PathType "Container")) {
+    New-Item -Path "C:\" -Name "temp" -ItemType "Directory" -Force | Out-Null
+}
 
 # Get latest version from Github releases
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 # Needed to avoid SSL/TLS error
 $url = 'https://github.com/PowerShell/Win32-OpenSSH/releases/latest/'
-$request = [System.Net.WebRequest]::Create($url)
-$request.AllowAutoRedirect = $false
-$response = $request.GetResponse()
-$downloadURL = $([String]$response.GetResponseHeader("Location")).Replace('tag', 'download') + '/OpenSSH-Win64.zip'
+try {
+    $response = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction "Stop"
+}
+catch [System.InvalidOperationException] {
+    continue
+}
+$responseHeaders = $response | Select-Object -ExpandProperty "Headers"
+$location = $responseHeaders.Item("Location")
+$downloadURL = $location.Replace('tag', 'download') + '/OpenSSH-Win64.zip'
 
 # Build desired file names
 $version = $downloadURL.Split('/')[7]
 $zippedFileName = "OpenSSH-Win64-$version.zip"
 $unzippedFileName = $zippedFileName.Replace(".zip", '')
 
-# Download the zip file, store in C:\Temp\
+# Download the zip file, store in C:\temp\
 Write-Host "Downloading version $version..."
 $client = New-Object System.Net.WebClient
-$client.DownloadFile($downloadURL, "C:\Temp\$zippedFileName")
+$client.DownloadFile($downloadURL, "C:\temp\$zippedFileName")
 
 # Unzip and copy to Program Files
 Write-Host "Unzipping archive and copying to Program Files..."
-Expand-Archive -Path "C:\Temp\$zippedFileName" -DestinationPath "C:\Temp\$unzippedFileName"
-Copy-Item -Path "C:\Temp\$unzippedFileName\OpenSSH-Win64" -Destination "$Env:ProgramFiles\OpenSSH" -Recurse
+Expand-Archive -Path "C:\temp\$zippedFileName" -DestinationPath "C:\temp\$unzippedFileName"
+Copy-Item -Path "C:\temp\$unzippedFileName\OpenSSH-Win64" -Destination "$Env:ProgramFiles\OpenSSH" -Recurse
 
 # Configuration
 Write-Host "Configuring services, inbound on port 22, and registry..."
@@ -50,7 +55,7 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name "BobkePSProfileScript" -Va
 # Cleanup
 Write-Host "Cleaning up..."
 $client.Dispose()
-Remove-Item -Path "C:\Temp\$zippedFileName"
-Remove-Item -Path "C:\Temp\$unzippedFileName" -Recurse
+Remove-Item -Path "C:\temp\$zippedFileName"
+Remove-Item -Path "C:\temp\$unzippedFileName" -Recurse
 
 Write-Host "Done!"
