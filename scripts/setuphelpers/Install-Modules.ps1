7def34d4445ca5be66d1579b1f4cb3b8ba93563a
Matthew Bobke <mcbobke@gmail.com>
2018-05-20 22:18:11 -0700

    Now using Update-Module for previously installed modules

--- scripts/setuphelpers/Install-Modules.ps1
+++ scripts/setuphelpers/Install-Modules.ps1
@@ -8,13 +8,14 @@ Write-Host "    Setting PSRepository 'PSGallery' to Trusted..."
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted' | Out-Null
 
 Write-Host "    Updating PowerShellGet module..."
-Install-Module -Name 'PowerShellGet' -Repository 'PSGallery' | Out-Null
+Update-Module -Name 'PowerShellGet' -Force | Out-Null
 Get-Module -Name 'PowerShellGet' | Remove-Module -Force | Out-Null
 Import-Module -Name 'PowerShellGet' -Force | Out-Null
 Import-PackageProvider -Name 'PowerShellGet' -Force | Out-Null
 
-Write-Host "    Installing modules..."
+Write-Host "    Installing/Updating modules..."
 foreach ($module in $Global:modulesToInstall) {
     Write-Host "        $module" -ForegroundColor Green
-    Install-Module -Name $module -Scope 'AllUsers'
+    if (!(Get-Module -Name $module)) {Install-Module -Name $module -Scope 'AllUsers' -Repository 'PSGallery'}
+    else {Update-Module -Name $module}
 }
