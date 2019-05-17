d24f6b233ce8e1fc68bebfb3204456ba0059c967
Matthew Bobke <mcbobke@gmail.com>
2019-05-16 17:00:09 -0700

    Added PSReadLine CaptureScreen binding for blog post usage

--- scripts/profile.ps1
+++ scripts/profile.ps1
@@ -56,3 +56,6 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
+
+# PSReadLine Options/Bindings
+Set-PSReadLineKeyHandler -Chord 'Ctrl+p' -Function CaptureScreen
